3ddce506b7364bb4057bf63867762e69b672b8bd
TheUbuntuUser <birthday@hints.or.kr>
2022-05-10 22:02:33 +0900

    fix

--- derp_j2y18lte.mk
+++ derp_j2y18lte.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/derp/config/common.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit MIUI Camera
 $(call inherit-product, vendor/MiuiCamera/config.mk)
