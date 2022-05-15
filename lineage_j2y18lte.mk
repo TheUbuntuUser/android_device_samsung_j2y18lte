8b40ef3f520a7c25b29f725f6d0daaa4b89ea8d9
TheUbuntuUser <birthday@hints.or.kr>
2022-05-15 21:28:59 +0900

    inherit MIUI Camera

--- lineage_j2y18lte.mk
+++ lineage_j2y18lte.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit MIUI Camera
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
 # Inherit from j2y18lte device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
